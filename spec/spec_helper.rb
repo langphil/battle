60a4f3c1f9a91558976a157fb0f9868236d9183d
Phil Lang <plangphoto@gmail.com>
2017-07-18 19:18:16 +0100

    added initial test for / page creation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,9 +14,22 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  ENV['RACK_ENV'] = 'test'
+
+  # require our Sinatra app file
+  require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+  require 'capybara'
+  require 'capybara/rspec'
+  require 'rspec'
+
+  # tell Capybara about our app class
+  Capybara.app = Battle
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -97,4 +110,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+# add this to spec/spec_helper.rb
 end
