2d5de8519290d03e66c1331e6650085f456d960a
Phil Lang <plangphoto@gmail.com>
2017-07-18 19:48:46 +0100

    tests for an entry form for two player names and submission

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,6 @@
 feature 'Testing infrastructutre' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    expect(page).to have_content 'Enter both players names'
   end
 end
