770451ce9970d6aee7d531b7c51de515c7d49cc2
Costas Kiteou <ckiteou22@gmail.com>
2017-07-19 13:20:16 +0100

    Refactored code for feature test for hit attack player confirmation

--- spec/features/player_hit_confirmation_spec.rb
+++ spec/features/player_hit_confirmation_spec.rb
@@ -1,5 +1,8 @@
 feature 'View hit comfirmation' do
   scenario 'confimr player 2 hit' do
     sign_in_and_play
+    visit('/play')
+    find_button('Attack').click
+    visit('/attack')
   end
 end
--- spec/features/testing_player_hit_points.rb
+++
@@ -1,7 +0,0 @@
-feature 'View hit points' do
-  scenario 'see player 2 hit points' do
-    sign_in_and_play
-    visit('/play')
-    find('.hit', text: '60')
-  end
-end
